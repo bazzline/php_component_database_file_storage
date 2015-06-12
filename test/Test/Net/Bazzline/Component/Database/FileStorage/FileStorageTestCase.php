bcb3951930d531ca303cde57c0e654842d7ba09f
stevleibelt <stev@leibelt.de>
2015-06-13 00:24:33 +0200

    added filesystem helpers

--- test/Test/Net/Bazzline/Component/Database/FileStorage/FileStorageTestCase.php
+++ test/Test/Net/Bazzline/Component/Database/FileStorage/FileStorageTestCase.php
@@ -9,6 +9,7 @@ namespace Test\Net\Bazzline\Component\Database\FileStorage;
 use Mockery;
 use Net\Bazzline\Component\Database\FileStorage\Repository;
 use Net\Bazzline\Component\Database\FileStorage\UUIDGenerator;
+use org\bovigo\vfs\vfsStream;
 use PHPUnit_Framework_TestCase;
 
 abstract class FileStorageTestCase extends PHPUnit_Framework_TestCase
@@ -19,12 +20,36 @@ abstract class FileStorageTestCase extends PHPUnit_Framework_TestCase
         parent::tearDown();
     }
 
+    /**
+     * @return \org\bovigo\vfs\vfsStreamDirectory
+     */
+    protected function getFileSystem()
+    {
+        return vfsStream::setup('test');
+    }
 
+    /**
+     * @param string $name
+     * @return \org\bovigo\vfs\vfsStreamFile
+     */
+    protected function getNewFile($name)
+    {
+        return vfsStream::newFile($name);
+    }
+
+    /**
+     * @param string $name
+     * @return \org\bovigo\vfs\vfsStreamDirectory
+     */
+    protected function getNewDirectory($name)
+    {
+        return vfsStream::newDirectory($name);
+    }
 
     /**
      * @return Repository
      */
-    protected function getNewRepositry()
+    protected function getNewRepository()
     {
         return new Repository();
     }
