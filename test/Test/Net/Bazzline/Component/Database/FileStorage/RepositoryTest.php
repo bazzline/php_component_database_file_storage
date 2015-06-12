5b6a7555af48f002f7b761c79baa4d9ddbd9fb81
stevleibelt <artodeto@bazzline.net>
2015-06-12 16:07:41 +0200

    prepared for RepositoryTest

--- test/Test/Net/Bazzline/Component/Database/FileStorage/RepositoryTest.php
+++ test/Test/Net/Bazzline/Component/Database/FileStorage/RepositoryTest.php
@@ -8,7 +8,42 @@ namespace Test\Net\Bazzline\Component\Database\FileStorage;
 
 class RepositoryTest extends FileStorageTestCase
 {
-    public function testFoo()
+    public function testCreate()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testReadMany()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testReadOne()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testUpdate()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testDelete()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testFilterBy()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testFilterById()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testLimitBy()
     {
         $this->markTestSkipped();
     }
